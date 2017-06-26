9a51e787305fd219ed61bb2e3b97532069ea1cac
maxbacinskiy <mbachinskiyol@gmail.com>
2017-06-26 03:27:53 +0300

    Макет

--- src/ru/dmop/Main.java
+++ src/ru/dmop/Main.java
@@ -2,12 +2,9 @@ package ru.dmop;
 
 import ru.dmop.windows.StartFrame;
 
-import javax.swing.*;
-
 public class Main {
 
     public static void main(String[] args) {
-        JFrame startWindow = new StartFrame();
-        startWindow.setVisible(true);
+        new StartFrame();
     }
 }
--- src/ru/dmop/windows/StartFrame.java
+++ src/ru/dmop/windows/StartFrame.java
@@ -1,6 +1,8 @@
 package ru.dmop.windows;
 
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  Стартовый экран
@@ -10,18 +12,38 @@ public class StartFrame extends JFrame {
         super("Поиск кратчайших путей в графе");
         Box box = Box.createVerticalBox();
         box.add(Box.createVerticalStrut(20));
+
         JButton randomGraph = new JButton("RandomGraph");
+        randomGraph.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                setVisible(false);
+                new RandomFrame();
+            }
+        });
         randomGraph.setAlignmentX(JComponent.CENTER_ALIGNMENT);
         box.add(randomGraph);
+
         box.add(Box.createVerticalStrut(10));
         box.add(Box.createVerticalGlue());
-        JButton Exit = new JButton("Exit");
-        Exit.setAlignmentX(JComponent.CENTER_ALIGNMENT);
-        box.add(Exit);
+
+        JButton exit = new JButton("Exit");
+        exit.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+        exit.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                setVisible(false);
+                dispose();
+                System.exit(0);
+            }
+        });
+        box.add(exit);
         box.add(Box.createVerticalStrut(20));
+
         setContentPane(box);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         pack();
+        setVisible(true);
     }
 
 }
